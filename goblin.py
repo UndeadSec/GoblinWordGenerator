0a121a6ebc3241416a90a6d8c7f27cbec5334e7f
Sam Junior <40306119+samjunior416@users.noreply.github.com>
2018-07-06 22:48:21 +0200

    Add files via upload

--- goblin.py
+++ goblin.py
@@ -1,26 +1,51 @@
+# Inteligent Wordlist Generator
+#
+# By: Sam Junior and UndeadSec
+# Goblin Wordlist Generator
+# Version: 2.0
+#
+#
+##################
+
 import itertools
 
-print('\n------------------\n\n G 0 B L ! N | WORDGENERATOR\n\n      ~ by: UndeadSec\n\n------------------\n')
 
-escala = input('Please provide a size scale(i.e: "1 to 5" = 1:5) : ')
-inicial = int(escala.split(':')[0])
-final = int(escala.split(':')[1])
+ban = '''
 
-chrs = 'abcdefghijklmnopqrstuvwxyz'
+                                                    '''
+
+print('\n------------------\n\n G 0 B L ! N \033[32m2.0\033[m | WORDGENERATOR\n\n~ by: UndeadSec and Sam Junior:@un00mz\n\n------------------\n')
+
+scale = input('\033[36m[!] provide a size scale [eg: "1 to 8" = 1:8] : ')
+start = int(scale.split(':')[0])
+final = int(scale.split(':')[1])
+
+use_nouse = str(input("\n\033[36m[?] Do you want to enter personal data ? [y/N]: "))
+if use_nouse == 'y':
+	first_name = str(input("\n\033[36m[*] Fist Name: "))
+	last_name = str(input("\n\033[36m[*] Last Name: "))
+	birthday = str(input("\n\033[36m[*] Birthday: "))
+	month = str(input("\n\033[36m[*] Month: "))
+	year = str(input("\n\033[36m[*] Year: "))
+	chrs = first_name + last_name + birthday + month + year
+else:
+	chrs = 'abcdefghijklmnopqrstuvwxyz'
+	pass
+
 chrs_up = chrs.upper()
-chrs_especiais = '!\][/?.,~-=";:><@#$%&*()_+\' '
-chrs_numericos = '1234567890'
+chrs_specials = '!\][/?.,~-=";:><@#$%&*()_+\' '
+chrs_numerics = '1234567890'
 
-arq_nome = input('Insert a name for your wordlist file: ')
-arq = open(arq_nome, 'w')
-if input('Do you want to use uppercase characters? (y/n): ') == 'y':
+file_name = input('\n\033[36m[!] Insert a name for your wordlist file: ')
+arq = open(file_name, 'w')
+if input('\n\033[36m[?] Do you want to use uppercase characters? (y/n): ') == 'y':
 	chrs = ''.join([chrs, chrs_up])
-if input('Do you want to use special characters? (y/n): ') == 'y':
-	chrs = ''.join([chrs, chrs_especiais])
-if input('Do you want to use numeric characters? (y/n): ') == 'y':
-	chrs = ''.join([chrs, chrs_numericos])
+if input('\n\033[36m[?] Do you want to use special characters? (y/n): ') == 'y':
+	chrs = ''.join([chrs, chrs_specials])
+if input('\n\033[36m[?] Do you want to use numeric characters? (y/n): ') == 'y':
+	chrs = ''.join([chrs, chrs_numerics])
 
-for i in range(inicial, final+1):
+for i in range(start, final+1):
 	for j in itertools.product(chrs, repeat=i):
 		temp = ''.join(j)
 		print(temp)
